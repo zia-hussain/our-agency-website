138991f109d91f8711043e26892ee487138dc953
zia-hussain <syedziashahgillani76@gmail.com>
2025-07-30 17:42:49 +0500

    Update my name to full name

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -180,7 +180,7 @@ const Footer: React.FC = () => {
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-muted-foreground text-sm mb-4 md:mb-0">
               © {currentYear} Zumetrix Labs. All rights reserved. Coded with ❤️
-              by Syed Zia Hussain and Syed Omer Shah.
+              by Syed Zia Hussain Shah and Syed Omer Shah.
             </p>
             <div className="flex space-x-6 text-sm">
               <Link
